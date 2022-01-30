c481f23651e6cc4564448ddaa66f5aa5a82acfff
Charlotte <chaaf@g.clemson.edu>
2022-01-29 21:06:56 -0500

    pretty splash page

--- frontend/src/pages/Splash.jsx
+++ frontend/src/pages/Splash.jsx
@@ -1,10 +1,16 @@
-import { Button, Container,Flex, Spacer, Img, Image } from "@chakra-ui/react";
+import { Button, Container,Flex, Spacer, Img, Image, Box, keyframes } from "@chakra-ui/react";
 import React from "react";
 import logo from '/images/logo.svg';
 
+/*const pan = keyframes`
+  from { transform: translate(0,0); }
+  to { transform: translate(-150px, 0); }
+`*/
+
 const Splash = () => {
+ //const moveAnimation = `${pan} infinite 5s linear`;
   return (
-    <Container h="100vh" maxW="container.xl" p={6} backgroundColor={"orange.300"}>
+    <Container h="100vh" maxW="container.xl" p={6} backgroundImage="url('public/images/cc_splash.jpg')"  backgroundPosition="left" backgroundRepeat="no-repeat" backgroundSize="cover">
       <Image py={100} src={logo} alt='College Cart'/>
       <Flex py={0}>
         <Spacer/>
